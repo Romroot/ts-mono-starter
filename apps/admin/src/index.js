ec81e9506f9196e185ac7986b1db93667b1d3c88
ra2dev <raramanouski@gmail.com>
2024-10-29 23:22:54 +0100

    Added webpack bundle

---
+++ apps/admin/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WEbpack hello world</h1>
+    </div>
+  );
+}
+const rootElement = document.getElementById("app");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
