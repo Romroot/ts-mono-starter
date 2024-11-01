58653a28779406e5cf5a2bc888cea0f94a92df1e
ra2dev <raramanouski@gmail.com>
2024-11-01 22:09:44 +0100

    Test change bundle

--- apps/next-app/app/page.tsx
+++ apps/next-app/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 
+import lodash from "lodash";
 import styles from "./page.module.css";
 
 export const dynamic = "force-dynamic";
@@ -19,6 +20,7 @@ export default function Home() {
         />
         <ol>
           <li>
+            {Object.keys(lodash)}
             Get started by editing <code>app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
